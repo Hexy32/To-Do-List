fe23b6579ef254971e8be3a27115fc2268c2ffd8
Hexy32 <sunshine.goody8762@gmail.com>
2023-10-09 21:26:24 -0400

    Updated github action

--- package.cjs
+++ package.cjs
@@ -1,35 +1,36 @@
 const packager = require('electron-packager')
 
-async function bundleElectronApp(options) {
-  const appPaths = await packager(options)
+async function bundleElectronApp() {
+  const appPaths = await packager({
+    dir: __dirname,
+    appBundleId: '',
+    appCopyright: 'Apache License',
+    appVersion: '1.0',
+    arch: 'x64',
+    executableName: 'todo',
+    icon: './public/assets/logos/128x128.ico',
+    name: 'todo',
+    out: 'build',
+    overwrite: true,
+    platform: ['win32', 'darwin', 'linux'],
+    prune: true,
+
+    ignore: [
+      '.vscode',
+      'build',
+      'node_modules',
+      'public',
+      'sass',
+      'src',
+      '.gitattributes',
+      '.gitignore',
+      'package-lock.json',
+      'tsconfig.json',
+      'README.md',
+      'LICENSE',
+    ],
+  })
   console.log(`Electron app bundles created:\n${appPaths.join('\n')}`)
 }
 
-bundleElectronApp({
-  dir: __dirname,
-  appBundleId: '',
-  appCopyright: 'Apache License',
-  appVersion: '1.0',
-  arch: 'x64',
-  executableName: 'todo',
-  icon: './public/assets/logos/128x128.ico',
-  name: 'To Do List',
-  out: 'build',
-  overwrite: 'true',
-  platform: ['win32'],
-  prune: true,
-  ignore: [
-    '.vscode',
-    'build',
-    'node_modules',
-    'public',
-    'sass',
-    'src',
-    '.gitattributes',
-    '.gitignore',
-    'package-lock.json',
-    'tsconfig.json',
-    'README.md',
-    'LICENSE',
-  ],
-})
+bundleElectronApp()
